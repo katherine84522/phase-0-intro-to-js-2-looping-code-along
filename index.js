25c6f81a7b0d48d16c7fd11d6c64bb6072350c97
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-10-12 15:51:48 -0400

    commit tree 08edfa744888421d79457616d97754d8e1d001a9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+const holiday = ["birthday"]
+
+function writeCards(names, holiday) {
+    const wishes = []
+    for (let i = 0; i < names.length; i++) {
+        wishes.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+    }
+    return wishes
+}
+
+writeCards(names, holiday);
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
+countDown();
